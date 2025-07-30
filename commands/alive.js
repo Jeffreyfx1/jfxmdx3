98eadee4bbc2fa05ad6261d7f492c7d8b7f27a5c
Jeffreyfx1 <jeffreyudenze@gmail.com>
2025-07-30 13:22:28 -0700

    Update alive.js

--- commands/alive.js
+++ commands/alive.js
@@ -1,7 +1,7 @@
 const settings = require("../settings");
 async function aliveCommand(sock, chatId, message) {
     try {
-        const message1 = `*🤖 𝒥𝐹𝒳 𝑀𝒟-𝒳 is Active!*\n\n` +
+        const message1 = `*🏴‍☠ 𝐉𝐅𝐗 𝐌𝐃-𝐗 is Active!*\n\n` +
                        `*Version:* ${settings.version}\n` +
                        `*Status:* Online\n` +
                        `*Mode:* Public\n\n` +
@@ -19,7 +19,7 @@ async function aliveCommand(sock, chatId, message) {
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
                     newsletterJid: '120363161513685998@newsletter',
-                    newsletterName: '𝒥𝐹𝒳 𝑀𝒟-𝒳',
+                    newsletterName: '𝐉𝐅𝐗 𝐌𝐃-𝐗',
                     serverMessageId: -1
                 }
             }
