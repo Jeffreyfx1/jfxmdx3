0faf60f12d4e98e7c8ecc47ea2feb8e8cbc3e9c6
Udenze Chibuzor Jephter <jeffreyudenze@gmail.com>
2025-07-30 06:06:37 -0700

    Initial push with customized JFX MD-X bot settings

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'JFX MD-X',
+  author: 'JEPHTER TECH',
+  botName: "JFX MD-X",
+  botOwner: 'JEPHTER TECH', // Your name
+  ownerNumber: '2349046157539', // No + symbol
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "A powerful WhatsApp MD bot by JEPHTER TECH. Channel: https://whatsapp.com/channel/0029VbAxkJl0lwgqAOojKI3R",
+  version: "2.0.8",
+};
+
+module.exports = settings;
