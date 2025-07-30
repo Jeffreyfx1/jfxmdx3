deca7242c4692509b2ca16122273c9a6cbd5d34d
Jeffreyfx1 <jeffreyudenze@gmail.com>
2025-07-30 09:39:23 -0700

    Update roseday.js

--- commands/roseday.js
+++ commands/roseday.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 
 async function rosedayCommand(sock, chatId, message) {
     try {
-        const shizokeys = 'knightbot';
+        const shizokeys = '𝒥𝐹𝒳 𝑀𝒟-𝒳';
         const res = await fetch(`https://api.shizo.top/quote/roseday?apikey=${shizokeys}`);
         
         if (!res.ok) {
